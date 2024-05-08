db9f8ce2a581e4988438ba30a9cc83512133a58e
Agbedeyisegun <pheemyhan@gmail.com>
2024-05-08 19:15:42 +0100

    added new app.py that os.environ.get() to to connect indirectly

--- app.py
+++ app.py
@@ -1,34 +1,27 @@
-from flask import Flask
-from flask import render_template
-import socket
-import mysql.connector
 import os
+from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
-
-DB_Host = os.environ.get('DB_Host') or "localhost"
-DB_Database = os.environ.get('DB_Database') or "mysql"
-DB_User = os.environ.get('DB_User') or "root"
-DB_Password = os.environ.get('DB_Password') or "paswrd"
 
 @app.route("/")
 def main():
     db_connect_result = False
     err_message = ""
     try:
-        mysql.connector.connect(host=DB_Host, database=DB_Database, user=DB_User, password=DB_Password)
+        conn = mysql.connector.connect(
+            host=os.environ.get('DB_Host'),
+            database=os.environ.get('DB_Database'),
+            user=os.environ.get('DB_User'),
+            password=os.environ.get('DB_Password')
+        )
         color = '#39b54b'
         db_connect_result = True
-    except Exception as e:
+    except mysql.connector.Error as e:
         color = '#ff3f3f'
         err_message = str(e)
 
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
-
-@app.route("/debug")
-def debug():
-    color = '#2196f3'
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set"), color=color)
+    return render_template('hello.html', debug=err_message, db_connect_result=db_connect_result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
